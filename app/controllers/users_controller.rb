eebca30d818d27039e04e9e30ef2f73bbd5a1ee4
bafiam <stevebafiam@gmail.com>
2020-02-29 00:00:55 +0300

    update user and error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,20 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+       redirect_to new_user_path 
+    else 
+      render :edit
+    end 
+  end
 
   private
 
